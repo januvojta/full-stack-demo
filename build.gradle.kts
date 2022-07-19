0b18df9b3e6ae4f88f1b1b93da53b0af4d2a1d05
Vojta <januvojta@gmail.com>
2022-07-19 16:36:38 +0200

    pruned dependencies for jvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,12 +52,9 @@ kotlin {
         }
         val backendMain by getting {
             dependencies {
-                implementation("org.springframework.boot:spring-boot-starter-data-rest")
                 implementation("org.jetbrains.kotlinx:kotlinx-html:0.7.5")
                 implementation("org.springframework.boot:spring-boot-starter-web")
-                implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-                implementation("org.jetbrains.kotlin:kotlin-reflect")
-                implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
 
             }
         }
